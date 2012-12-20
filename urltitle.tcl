8adce8a2d6808d7ccc5c0a986d7d7a61f7ae96dc
The-Sky <the.skylarr@gmail.com>
2012-12-20 01:00:32 -0600

    Added Main Channel Support

--- urltitle.tcl
+++ urltitle.tcl
@@ -1,61 +1,61 @@
-# Usage: 
+# Usage:
 
 # 1) Set the configs below
-# 2) .chanset #channelname +urltitle        ;# enable script
-# 3) .chanset #channelname +logurltitle     ;# enable logging
+# 2) .chanset #channelname +urltitle ;# enable script
+# 3) .chanset #channelname +logurltitle ;# enable logging
 # Then just input a url in channel and the script will retrieve the title from the corresponding page.
 
 ################################################################################################################
 
 # Configs:
 
-set urltitle(ignore) "bdkqr|dkqr" 	;# User flags script will ignore input from
-set urltitle(pubmflags) "-|-" 		;# user flags required for channel eggdrop use
-set urltitle(length) 5	 		;# minimum url length to trigger channel eggdrop use
-set urltitle(delay) 1 			;# minimum seconds to wait before another eggdrop use
-set urltitle(timeout) 60000 		;# geturl timeout (1/1000ths of a second)
+set urltitle(ignore) "bdkqr|dkqr" ;# User flags script will ignore input from
+set urltitle(pubmflags) "-|-" ;# user flags required for channel eggdrop use
+set urltitle(length) 5	;# minimum url length to trigger channel eggdrop use
+set urltitle(delay) 1 ;# minimum seconds to wait before another eggdrop use
+set urltitle(timeout) 60000 ;# geturl timeout (1/1000ths of a second)
 
 ################################################################################################################
 
 # Script begins:
 
-package require http			;# You need the http package..
-set urltitle(last) 111 			;# Internal variable, stores time of last eggdrop use, don't change..
-setudef flag urltitle			;# Channel flag to enable script.
-setudef flag logurltitle		;# Channel flag to enable logging of script.
+package require http ;# You need the http package..
+set urltitle(last) 111 ;# Internal variable, stores time of last eggdrop use, don't change..
+setudef flag urltitle ;# Channel flag to enable script.
+setudef flag logurltitle ;# Channel flag to enable logging of script.
 
 set urltitlever "0.01a"
+set message ""
 
 bind pubm $urltitle(pubmflags) {*://*} pubm:urltitle
 proc pubm:urltitle {nick host user chan text} {
-	global urltitle this
+	global urltitle this message
 	set response ""
-	if {([channel get $chan urltitle]) && ([expr [unixtime] - $urltitle(delay)] > $urltitle(last)) && \
-	(![matchattr $user $urltitle(ignore)])} {
-		foreach word [split $text] {
-			if {[string length $word] >= $urltitle(length) && [regexp {^(f|ht)tp(s|)://} $word] && ![regexp {://([^/:]*:([^/]*@|\d+(/|$))|.*/\.)} $word]} {
-				set urltitle(last) [unixtime]
-				set urtitle [urltitle $word]
-				if {[string length $urtitle]} {
-					set message "http://hidemyass.com/?$this"
-					string trim $message
-					set out [maketiny $message $nick $chan]
-					set response "$out"
-					puthelp "PRIVMSG $chan :\002$urtitle\002 - $response"
+	if {$nick != "Hobbes"} {
+		if {([channel get $chan urltitle]) && ([expr [unixtime] - $urltitle(delay)] > $urltitle(last)) && \
+				(![matchattr $user $urltitle(ignore)])} {
+			foreach word [split $text] {
+				if {[string length $word] >= $urltitle(length) && [regexp {^(f|ht)tp(s|)://} $word] && ![regexp {://([^/:]*:([^/]*@|\d+(/|$))|.*/\.)} $word]} {
+						if {![string match {*torrent-invites.com*} $word]} {
+						set urltitle(last) [unixtime]
+						set urtitle [urltitle $word]
+						if {[string length $urtitle]} {
+							set message "http://hidemyass.com/?$word"
+							string trim $message
+							set out [maketiny $message $nick $chan]
+							set response "$out"
+							if {$chan == "#torrent-invites"} {
+								puthelp "PRIVMSG #BotDev :Zucchini: $urtitle - $response"
+							} else {
+								puthelp "PRIVMSG $chan : $urtitle - $response"
+							}
+						}
+						break
+					}
 				}
-				break
-			}
-		}
-        }
-	if {[channel get $chan logurltitle]} {
-		foreach word [split $text] {
-			if {[string match "*://*" $word]} {
-				putlog "<$nick:$chan> $word -> $urtitle"
 			}
 		}
 	}
-	# change to return 0 if you want the pubm trigger logged additionally..
-	return 1
 }
 
 proc urltitle {url} {
@@ -95,6 +95,6 @@ proc maketiny {url nick chan} {
 		}
 	}
 	putlog "tinyurl failed for $nick: $user_url"
-	}
+}
 
 putlog "Url Title Grabber $urltitlever (rosc) script loaded.."
